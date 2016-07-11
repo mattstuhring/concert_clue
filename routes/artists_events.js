2226a2b6e88a0f9d050870962ae2124e02b73373
Matt Stuhring <whereyouatmatt@gmail.com>
2016-07-10 18:04:09 -0700

    premptive branch

--- routes/artists_events.js
+++ routes/artists_events.js
@@ -1,3 +1,20 @@
+'use strict';
+
+const express = require('express');
+const router = express.Router();
+const knex = require('../knex');
+const ev = require('express-validation');
+const validations = require('../validations/joiartists_events');
+
+router.get('/artists/events', (req, res, next) => {
+
+
+
+});
+
+
+module.exports = router;
+
 /* handling get request to ./artists/events
 
 A properly formed get request to /artist/events must be an array of mbid's in the body.
@@ -6,7 +23,4 @@ The body will simply JSON.parse()  it will be a jsonrequest as the body.  we'll 
 
 if we don't receive a properly formed body in the request, we'll respond with a 401 error.
 
-
-
-
-router.get('/artists/events')
+*/
