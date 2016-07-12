e470e42ca9de98f339969063004e8cb29f26dfc9
Chad Latham <chadlatham33@gmail.com>
2016-07-11 18:30:32 -0700

    Update validation of artists/events route

--- validations/joiartists_events.js
+++ validations/joiartists_events.js
@@ -5,11 +5,26 @@ const Joi = require('joi');
 module.exports.get = {
   body: {
     artists: Joi.array()
+      .label('Artists')
       .items(
         Joi.string()
         .label('Artist Name')
         .required()
+        .max(255)
         .trim()
-      )
+      ),
+    city: Joi.string()
+      .label('City')
+      .max(255)
+      .trim(),
+    state: Joi.string()
+      .label('State')
+      .trim()
+      .length(2),
+    radius: Joi.number()
+      .label('Radius')
+      .integer()
+      .min(1)
+      .max(150)
   }
 };
