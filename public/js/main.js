5a0440cedeec589e005ef56dda8471de0807e200
Patrick Richardson <setfloat@gmail.com>
2016-07-16 17:15:45 -0700

    mainjs main linting

--- public/js/main.js
+++ public/js/main.js
@@ -1,6 +1,5 @@
 'use strict';
-(function () {
-
+(function() {
   let favorites = [];
   let events = [];
   let searchText = '';
@@ -18,14 +17,17 @@
     const $favart = $('.favart');
 
     $favart.children().remove();
-    $favart.append(`<li class="collection-header"><h4>Favorite Artist</h4></li>`);
+    $favart.append(`<li class="collection-header">
+    <h4>Favorite Artist</h4></li>`);
 
     for (const artist of favorites) {
       $favart.append(
         `<li class="collection-item avatar">
-          <a href="${artist.facebook_page_url}"><img src="${artist.thumb_url}" alt="artist pic" class="circle"></a>
+          <a href="${artist.facebook_page_url}"><img
+          src="${artist.thumb_url}" alt="artist pic" class="circle"></a>
           <span class="title">${artist.name}</span>
-          <a class="secondary-content ccauid" ccauid="${artist.id}"><i class="material-icons">cancel</i></a>
+          <a class="secondary-content ccauid"
+          ccauid="${artist.id}"><i class="material-icons">cancel</i></a>
         </li>`
       );
     }
@@ -35,8 +37,9 @@
     const $eventAddCard = $('.eventAddCard');
 
     $eventAddCard.children().remove();
-    for(const event of events) {
-      $eventAddCard.append(`<div class="col s12 barley valign-wrapper z-depth-3">
+    for (const event of events) {
+      $eventAddCard.append(`<div class="col s12 barley valign-wrapper
+      z-depth-3">
         <div class="hops valign">
           <p></p>
           <span>${event.artists[0].name}</span>
@@ -53,10 +56,7 @@
     const $xhr = $.ajax({
       method: 'DELETE',
       url: '/session/',
-      // dataType: 'json',
       contentType: 'application/json'
-      // data: '{"title": "Frozen 2: The Thaw", "rating": 6.8}'
-      // data: JSON.stringify(newUser)
     });
 
     $xhr.done(() => {
@@ -73,7 +73,6 @@
       method: 'GET',
       url: '/users/artists',
       contentType: 'application/json'
-      // data: JSON.stringify()
     })
     .done((artists) => {
       favorites = artists;
@@ -82,6 +81,7 @@
       let city;
       let state;
       let radius;
+
       $.ajax({
         method: 'GET',
         url: '/users/',
@@ -95,36 +95,35 @@
         $.ajax({
           method: 'POST',
           url: '/artists/events/',
-          // processData: false,
           contentType: 'application/json',
           data: JSON.stringify({
             artists: artistNames,
-            city: city,
-            state: state,
-            radius: radius
+            city,
+            state,
+            radius
           })
         })
         .done((localEvents) => {
-          events = _.sortBy(localEvents, (o) => o.datetime);
+          events = _.sortBy(localEvents, (object) => object.datetime);
           buildEvents();
         })
-        .fail(() => {
-          return Materialize.toast('We are here', 3000, 'rounded');
-        })
+        .fail(
+          Materialize.toast('We are here', 3000, 'rounded')
+        );
       })
-      .fail(() => {
-        return Materialize.toast('We are fail', 3000, 'rounded');
-      })
+      .fail(Materialize.toast('We are fail', 3000, 'rounded')
+      );
     })
     .fail(() => {
       Materialize.toast('Is everything hooked up alright?', 3000, 'rounded');
     });
-  }
+  };
 
   const remFavArt = function(event) {
     const $a = $(event.target).parent();
     const ccauid = Number.parseInt($a.attr('ccauid'));
     let index;
+
     for (let i = 0; i < favorites.length; i++) {
       if (favorites[i].id === ccauid) {
         index = i;
@@ -135,20 +134,18 @@
     const $xhr = $.ajax({
       method: 'DELETE',
       url: '/users/artists',
-      // dataType: 'json',
       contentType: 'application/json',
-      // data: '{"title": "Frozen 2: The Thaw", "rating": 6.8}'
       data: JSON.stringify({ ccauid })
     });
 
     $xhr.done((data) => {
-      console.log(data);
       favorites.splice(index, 1);
       buildFavorites();
       const artistNames = favorites.map((art) => art.name);
       let city;
       let state;
       let radius;
+
       $.ajax({
         method: 'GET',
         url: '/users/',
@@ -165,22 +162,20 @@
           contentType: 'application/json',
           data: JSON.stringify({
             artists: artistNames,
-            city: city,
-            state: state,
-            radius: radius
+            city,
+            state,
+            radius
           })
         })
         .done((localEvents) => {
-          events = _.sortBy(localEvents, (o) => o.datetime);
+          events = _.sortBy(localEvents, (object) => object.datetime);
           buildEvents();
         })
-        .fail(() => {
-          return Materialize.toast('We are here!!!!!!', 3000, 'rounded');
-        })
+        .fail(Materialize.toast('We are here!!!!!!', 3000, 'rounded')
+        );
       })
-      .fail(() => {
-        return Materialize.toast('We are fail', 3000, 'rounded');
-      })
+      .fail(Materialize.toast('We are fail', 3000, 'rounded')
+      );
     });
 
     $xhr.fail((err) => {
@@ -192,9 +187,7 @@
         Materialize.toast('Try again later', 3000, 'rounded');
       }
     });
-
   };
-
 
   const searchResponseCard = function(artist) {
     const $searchResponseCard = $('.searchResponseCard');
@@ -217,19 +210,19 @@
               <div class="card-action">
                 <div class="row">
                   <div class="col s8">
-                    <h5><a mbid="${artist.mbid}" class="addFave" href="#">Add to Favorites<h5><h5>
+                    <h5><a mbid="${artist.mbid}" class="addFave"
+                    href="#">Add to Favorites<h5><h5>
                   </div>
                 <div class="col s4">
-                  <a href="${artist.facebook_page_url}"><img src="/images/fb30.png"></a>
+                  <a href="${artist.facebook_page_url}"><img
+                  src="/images/fb30.png"></a>
                 </div>
               </div>
             </div>
           </div>
         </div>
     </div>`
-    )
-      console.log(artist);
-      console.log('cardbuilt');
+  );
   };
 
   const search = function(event) {
@@ -263,31 +256,31 @@
       $xhr.done((data) => {
         const { city, state, radius } = data;
 
-        const $xhr = $.ajax({
+        const $xhrin = $.ajax({
           method: 'POST',
           url: '/artists/events',
           contentType: 'application/json',
           data: JSON.stringify({
             artists: [searchText],
-            city: city,
-            state: state,
-            radius: radius
+            city,
+            state,
+            radius
           })
         });
 
-        $xhr.done((data) => {
-          console.log(data);
-          if(data.length === 0) {
+        $xhrin.done((data) => {
+          if (data.length === 0) {
             events = [];
             buildEvents();
+
             return Materialize.toast('No upcoming shows', 3000, 'rounded');
           }
 
-          events = _.sortBy(data, (o) => o.datetime);
+          events = _.sortBy(data, (object) => object.datetime);
           buildEvents();
         });
 
-        $xhr.fail((err) => {
+        $xhrin.fail((err) => {
           if (err.status === 404) {
             return Materialize.toast('Artist not found', 3000, 'rounded');
           }
@@ -309,10 +302,9 @@
 
       Materialize.toast('Hmmm, try again later', 3000, 'rounded');
     });
-  }
+  };
 
   const addToFavorites = function(event) {
-    console.log(event.target);
     $.ajax({
       method: 'POST',
       url: '/users/artists',
@@ -332,28 +324,28 @@
 
       $xhr.done((data) => {
         const { city, state, radius } = data;
-        const faveNames = favorites.map((arts) => arts.name)
-          const $xhr = $.ajax({
+        const faveNames = favorites.map((arts) => arts.name);
+        const $xhr = $.ajax({
           method: 'POST',
           url: '/artists/events',
           contentType: 'application/json',
           data: JSON.stringify({
             artists: faveNames,
-            city: city,
-            state: state,
-            radius: radius
+            city,
+            state,
+            radius
           })
         });
 
         $xhr.done((data) => {
-          console.log(data);
-          if(data.length === 0) {
+          if (data.length === 0) {
             events = [];
             buildEvents();
+
             return Materialize.toast('No upcoming shows', 3000, 'rounded');
           }
 
-          events = _.sortBy(data, (o) => o.datetime);
+          events = _.sortBy(data, (object) => object.datetime);
           buildEvents();
         });
 
@@ -371,7 +363,7 @@
       });
     })
     .fail((err) => {
-      if(err.status === 406) {
+      if (err.status === 406) {
         return Materialize.toast('Dang, you must like them!', 3000, 'rounded');
       }
       Materialize.toast('We have a problem, try again', 3000, 'rounded');
@@ -387,32 +379,32 @@
 
     $xhr.done((data) => {
       const { city, state, radius } = data;
-      const faveNames = favorites.map((arts) => arts.name)
-        const $xhr = $.ajax({
+      const faveNames = favorites.map((arts) => arts.name);
+      const $xhrzin = $.ajax({
         method: 'POST',
         url: '/artists/events',
         contentType: 'application/json',
         data: JSON.stringify({
           artists: faveNames,
-          city: city,
-          state: state,
-          radius: radius
+          city,
+          state,
+          radius
         })
       });
 
-      $xhr.done((data) => {
-        console.log(data);
-        if(data.length === 0) {
+      $xhrzin.done((data) => {
+        if (data.length === 0) {
           events = [];
           buildEvents();
+
           return Materialize.toast('No upcoming shows', 3000, 'rounded');
         }
 
-        events = _.sortBy(data, (o) => o.datetime);
+        events = _.sortBy(data, (object) => object.datetime);
         buildEvents();
       });
 
-      $xhr.fail((err) => {
+      $xhrzin.fail((err) => {
         if (err.status === 404) {
           return Materialize.toast('Artist not found', 3000, 'rounded');
         }
@@ -439,4 +431,3 @@
 
 // End IFFE - Must be at bottom of file.
 })();
-// `<a href="${TBD}" class="collection-item">${ArtistName}</a>`
